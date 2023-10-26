77e081e1393f27c5d5ab07a6e89104ceb0fa2da5
Chandu154 <devarachandra4@gmail.com>
2023-10-26 16:00:14 +0530

    first commit

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
     var stat = {
-        app: 'OKk',
+        app: 'OKk-2',
         mongo: mongoConnected
     };
     res.json(stat);
